6ddb1f20c352c0dd0eb082f1747c792ba98b8304
B1ackin <xx13.02@gmail.com>
2022-01-30 15:45:30 +0300

    add main block

---
+++ src/main/Main.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from './Main.module.css';
+
+
+function Main() {
+    return (
+        <div className={style.mainBlock}>
+            <div className={style.container}>
+                <div className={style.text}>
+                    <span>HELLO</span>
+                    <h1>I’M LEONID KRUCHENOK</h1>
+                    <p>I've been doing web design, front-end and back-end development for a year now.
+                        Do you need a website design, site layout, or maybe a turnkey website?
+                        Then contact me
+                    </p>
+                    <button className={style.btnDonwload}>DOWNLOAD CV</button>
+                </div>
+                <div className={style.photo}><img src={"/image/skillsAndPhoto.png"} /></div>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
---
+++ src/nav/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './Nav.module.css';
+
+function Nav() {
+    return (
+        <div className={style.nav}>
+            <a href="">Home</a>
+            <a href="">Skills</a>
+            <a href="">About me</a>
+            <a href="">Portfolio</a>
+            <a href="">Contact</a>
+        </div>
+    );
+}
+
+export default Nav;
